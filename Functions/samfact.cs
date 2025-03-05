6feab421c967585a6fd41be26b4a249af88bf8ea
nagendramishr <81572024+nagendramishr@users.noreply.github.com>
2025-03-05 16:50:30 +0000

    add lowercase id, initialize id

--- Functions/samfact.cs
+++ Functions/samfact.cs
@@ -4,5 +4,20 @@ namespace Functions
     public class SamFact {
         public string Message {get; set; }
         public string Id { get; set; }
+        public string id { get; set; }
+        public SamFact()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+        public SamFact(string message)
+        {
+            Message = message;
+            Id = Guid.NewGuid().ToString();
+        }
+        public SamFact(string message, string id)
+        {
+            Message = message;
+            Id = id;
+        }
     }
 }
