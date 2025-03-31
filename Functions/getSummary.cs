432298f8beb24d5d1e734620938ca55b8156cd91
nagendramishr <81572024+nagendramishr@users.noreply.github.com>
2025-03-31 18:19:47 +0000

    added check for deleted

--- Functions/getSummary.cs
+++ Functions/getSummary.cs
@@ -35,7 +35,7 @@ namespace Functions
             [CosmosDBInput(
                 databaseName: "%CosmosDb%",
                 containerName: "%CosmosContainer%",
-                SqlQuery = "SELECT * FROM c",
+                SqlQuery = "SELECT * FROM c where c.Deleted = false",
                 Connection = "dbstr")] IEnumerable<SamFact> allFacts,
             ILogger log
         )
