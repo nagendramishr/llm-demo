4370041e7b4a2cc6bed73b7d01e69f8285b9ec83
nagendramishr <81572024+nagendramishr@users.noreply.github.com>
2025-05-12 15:26:54 +0000

    add route

--- Functions/getFacts.cs
+++ Functions/getFacts.cs
@@ -19,7 +19,7 @@ namespace Functions
         //[Function("HttpFunction")]
         [Function("getFacts")]
         public IActionResult Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route ="{bid}")] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route ="getFacts/{bid}")] HttpRequest req,
             [CosmosDBInput(
                 databaseName: "%CosmosDb%",
                 containerName: "%CosmosContainer%",
